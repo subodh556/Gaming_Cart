dc2b9e07691dcc4f84d5d5ef26cfff267ea1dacc
Subodh <sohonisubodh@gmail.com>
2024-01-04 12:28:10 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+
 module.exports = {
+  ...nextConfig,
   images: {
     domains: ['imgur.com'],
   },
 };
-module.exports = nextConfig
