0ecf79cc25340496057fd650e9ef068413a5efdd
Subodh <sohonisubodh@gmail.com>
2024-01-04 10:47:26 +0530

    Navbar and Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
+
+export default MyApp;
