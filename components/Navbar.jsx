4b0ad42b10197bd08bea3fc9b8f91b9d8f0831c6
Subodh <sohonisubodh@gmail.com>
2024-01-13 14:13:04 +0530

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,28 @@
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import styles from '../styles/Navbar.module.css';
+import { useRouter } from 'next/router';
+import Image from 'next/image'
 const Navbar = () => {
 
   // Selecting cart from global state
   const cart = useSelector((state) => state.cart);
-
+  const router =useRouter();
   // Getting the count of items
   const getItemsCount = () => {
     return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
   };
-
+  const click=()=>{
+    router.push('/');
+  }
   return (
     <nav className={styles.navbar}>
-      <h6 className={styles.logo}>GamesKart</h6>
+      <div>
+      
+      
+      </div>
+      <Image className={styles.logo} onClick={click} src={"/logo1.png"} width={200} height={300} alt=""/>
+      
       <ul className={styles.links}>
         <li className={styles.navlink}>
           <Link href="/">Home</Link>
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ const HomePage = () => {
   return (
     <main className={styles.container}>
       <div className={styles.small}>
-        <CategoryCard image="/Xbox.jpg" name="Xbox" />
+       
         <CategoryCard image="/ps5.jpg" name="PS5" />
         <CategoryCard image="/switch.jpg" name="Switch" />
         
         <CategoryCard image="/pc.jpg" name="PC" />
         <CategoryCard image="/acce.jpg" name="Accessories"/>
+        <CategoryCard image="/Xbox.jpg" name="Xbox" />
       
       
       </div>
