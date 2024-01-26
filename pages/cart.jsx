b96818a690938d7090e3a3da46c318ca137dc5b8
Subodh <sohonisubodh@gmail.com>
2024-01-26 19:45:30 +0530

    commit

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -22,45 +22,7 @@ const CartPage = () => {
   };
 
   return (
-    <div className={styles.container}>
-      {cart.length === 0 ? (
-        <h1>Your Cart is Empty!</h1>
-      ) : (
-        <>
-          <div className={styles.header}>
-            <div>Image</div>
-            <div>Product</div>
-            <div>Price</div>
-            <div>Quantity</div>
-            <div>Actions</div>
-            <div>Total Price</div>
-          </div>
-          {cart.map((item) => (
-            <div className={styles.body}>
-              <div className={styles.image}>
-                <Image src={item.image} height="90" width="65"alt='' />
-              </div>
-              <p>{item.product}</p>
-              <p>$ {item.price}</p>
-              <p>{item.quantity}</p>
-              <div className={styles.buttons}>
-                <button onClick={() => dispatch(incrementQuantity(item.id))}>
-                  +
-                </button>
-                <button onClick={() => dispatch(decrementQuantity(item.id))}>
-                  -
-                </button>
-                <button onClick={() => dispatch(removeFromCart(item.id))}>
-                  x
-                </button>
-              </div>
-              <p>$ {item.quantity * item.price}</p>
-            </div>
-          ))}
-          <h2>Grand Total: $ {getTotalPrice()}</h2>
-        </>
-      )}
-    </div>
+    
   );
 };
 
